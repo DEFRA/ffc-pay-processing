4cc23a1128f99c2905f6d39fcaa689c6206c3519
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2025-04-17 14:10:08 +0100

    adding Y1 to getPaymentSchedule for R2 change (#242)
    * adding Y1 to getPaymentSchedule for R2 change
    
    * 2.55.44
    
    ---------
    
    Co-authored-by: ffcplatform <ffcplatforms@environment-agency.gov.uk>

--- app/processing/due-dates/get-payment-schedule.js
+++ app/processing/due-dates/get-payment-schedule.js
@@ -1,6 +1,6 @@
 const moment = require('moment')
 const { getSchedule } = require('./get-schedule')
-const { Q4, Q3, M12, T4, Y2, T2 } = require('../../constants/schedules')
+const { Q4, Q3, M12, T4, Y1, Y2, T2 } = require('../../constants/schedules')
 const { MONTH, DAY } = require('../../constants/time-periods')
 const { DATE_FORMAT } = require('../../constants/date-formats')
 
@@ -16,6 +16,8 @@ const getPaymentSchedule = (schedule, dueDate, settledValue, totalValue, current
       return getSchedule(scheduleDate, 12, settledValue, totalValue, 1, MONTH, currentDate)
     case T4:
       return getSchedule(scheduleDate, 4, settledValue, totalValue, 3, DAY, currentDate)
+    case Y1:
+      return getSchedule(scheduleDate, 1, settledValue, totalValue, 0, DAY, currentDate)
     case Y2:
       // Y2 schedule to be one payment on 01/08, second 30/09.
       return getSchedule(scheduleDate, 2, settledValue, totalValue, 60, DAY, currentDate)
