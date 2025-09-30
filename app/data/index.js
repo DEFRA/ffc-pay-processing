8b2d560d7d0ab8f52b67a54ad6d53391e4a0a800
John Barnard <john.barnard.external@eviden.com>
2025-09-30 10:05:19 +0100

    Lint fix
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/index.js
+++ app/data/index.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { databaseConfig: dbConfig} = require('../config')
+const { databaseConfig: dbConfig } = require('../config')
 const modelPath = path.join(__dirname, 'models')
 const { Database } = require('ffc-database')
 
