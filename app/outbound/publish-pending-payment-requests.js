e4ce9d2922174d18a7cdc5db59d55490fc69b721
Leigh Godson <leigh.godson@atos.net>
2025-11-10 11:19:05 +0000

    move events before finalisation messages

--- app/outbound/publish-pending-payment-requests.js
+++ app/outbound/publish-pending-payment-requests.js
@@ -15,9 +15,9 @@ const publishPendingPaymentRequests = async (submitted = new Date()) => {
     if (paymentRequests.length) {
       const messages = paymentRequests.map(message => createMessage(message, PROCESSED))
       const sender = new MessageBatchSender(messageConfig.submitTopic)
+      await sendPublishingEvents(paymentRequests)
       await sender.sendBatchMessages(messages)
       await sender.closeConnection()
-      await sendPublishingEvents(paymentRequests)
       await updatePendingPaymentRequests(paymentRequests, submitted, transaction)
       console.log('Payment requests processed:', util.inspect(messages.map(x => x.body), false, null, true))
     }
