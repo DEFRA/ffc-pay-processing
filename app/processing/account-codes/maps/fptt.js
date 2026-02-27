ee107c2cd9735581a80d5df332d37104da48455e
Sam Plackett <samuel.plackett@eviden.com>
2026-02-27 14:45:22 +0000

    add account codes

--- app/processing/account-codes/maps/fptt.js
+++ app/processing/account-codes/maps/fptt.js
@@ -1,69 +1,51 @@
-// placeholder map - data requested from Finance team to ensure correct mapping
-const { G00, G01, P02, P03, P05, P06, P08, P11, P14, P22, P23, P24, R00 } = require('../../../constants/line-codes')
+const { SOS710, SOS928, SOS921, SOS922, SOS923, SOS924, SOS925, SOS926, SOS927 } = require('../../../constants/account-codes/ap')
+const { SOS750, SOS948, SOS941, SOS942, SOS943, SOS944, SOS945, SOS946, SOS947 } = require('../../../constants/account-codes/ar-admin')
+const { SOS770, SOS968, SOS961, SOS962, SOS963, SOS964, SOS965, SOS966, SOS967 } = require('../../../constants/account-codes/ar-irregular')
+const { G00, P02, P05, P06, P08, P14, P22, P23, P24 } = require('../../../constants/line-codes')
 
 module.exports = [{
   lineCode: G00,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
-}, {
-  lineCode: G01,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
+  ap: SOS710,
+  arAdmin: SOS750,
+  arIrregular: SOS770
 }, {
   lineCode: P02,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
-}, {
-  lineCode: P03,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
+  ap: SOS928,
+  arAdmin: SOS948,
+  arIrregular: SOS968
 }, {
   lineCode: P05,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
+  ap: SOS921,
+  arAdmin: SOS941,
+  arIrregular: SOS961
 }, {
   lineCode: P06,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
+  ap: SOS922,
+  arAdmin: SOS942,
+  arIrregular: SOS962
 }, {
   lineCode: P08,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
-}, {
-  lineCode: P11,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
+  ap: SOS923,
+  arAdmin: SOS943,
+  arIrregular: SOS963
 }, {
   lineCode: P14,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
+  ap: SOS924,
+  arAdmin: SOS944,
+  arIrregular: SOS964
 }, {
   lineCode: P22,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
+  ap: SOS925,
+  arAdmin: SOS945,
+  arIrregular: SOS965
 }, {
   lineCode: P23,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
+  ap: SOS926,
+  arAdmin: SOS946,
+  arIrregular: SOS966
 }, {
   lineCode: P24,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
-}, {
-  lineCode: R00,
-  ap: '???',
-  arAdmin: '???',
-  arIrregular: '???'
+  ap: SOS927,
+  arAdmin: SOS947,
+  arIrregular: SOS967
 }]
