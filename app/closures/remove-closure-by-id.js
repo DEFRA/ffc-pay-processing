3eef8e49df98bc5acf1f9ca753fc321ed8df5309
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2026-07-24 16:28:11 +0100

    Remove suppression API links (#300)

--- app/closures/remove-closure-by-id.js
+++
@@ -1,9 +0,0 @@
-const db = require('../data')
-
-const removeClosureById = async (closedId) => {
-  await db.frnAgreementClosed.destroy({ where: { closedId } })
-}
-
-module.exports = {
-  removeClosureById
-}
