f7b512ecece8202186bd5f0149a78bbee07a9361
Sam Plackett <samuel.plackett@eviden.com>
2026-08-10 10:46:47 +0100

    Resolve issue with Acknowledgement alerts

--- app/acknowledgement/process-invalid.js
+++ app/acknowledgement/process-invalid.js
@@ -13,6 +13,8 @@ const processInvalid = async (paymentRequest, acknowledgement) => {
     const holdCategoryName = getHoldCategoryName(acknowledgement.message)
     const holdCategoryId = await getHoldCategoryId(schemeId, holdCategoryName, transaction)
     await holdAndReschedule(paymentRequestId, holdCategoryId, frn, transaction)
+    console.log(holdCategoryName)
+    console.log(acknowledgement)
     await sendAcknowledgementErrorEvent(holdCategoryName, acknowledgement, paymentRequest)
 
     await transaction.commit()
--- app/event/send-ack-error-event.js
+++ app/event/send-ack-error-event.js
@@ -15,6 +15,7 @@ const sendProcessingAckErrorEvent = async (acknowledgement) => {
       ...paymentRequest
     }
   }
+  console.log(event)
   const eventPublisher = new EventPublisher(messageConfig.eventsTopic)
   await eventPublisher.publishEvent(event)
 }
