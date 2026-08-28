ee3ab8d540a17ba5e9903bc80d8f312fb8e786a7
Oliver Lewington <oliver.a.lewington@gmail.com>
2026-08-28 11:04:40 +0100

    adding new duplicate payment event (warning)

--- app/inbound/get-existing-payment-request.js
+++ app/inbound/get-existing-payment-request.js
@@ -2,7 +2,7 @@ const db = require('../data')
 
 const getExistingPaymentRequest = async (invoiceNumber, transaction) => {
   return db.paymentRequest.findOne({
-    attributes: ['paymentRequestId', 'invoiceNumber'],
+    attributes: ['paymentRequestId', 'invoiceNumber', 'referenceId'],
     transaction,
     where: {
       invoiceNumber
