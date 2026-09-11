121fc04bf6ac078e77154654c86bfc8b20cc585f
awishart1989 <alistair.wishart@atos.net>
2026-09-11 12:58:11 +0100

    SFI26 Onboarding

--- test/integration/local/schemes/get-schemes.test.js
+++ test/integration/local/schemes/get-schemes.test.js
@@ -10,7 +10,7 @@ describe('get schemes', () => {
 
   test('should return all schemes that exist', async () => {
     const schemes = await getSchemes()
-    expect(schemes).toHaveLength(14)
+    expect(schemes).toHaveLength(15)
   })
 
   afterAll(async () => {
